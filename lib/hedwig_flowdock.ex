e701b25bc0bc31b17c06b214baa809dabe510e4d
Steve Grossi <me@stevegrossi.com>
2016-12-23 06:05:06 -0500

    Debugging lines
    Hedwig boots but doesn’t respond to anything

--- lib/hedwig_flowdock.ex
+++ lib/hedwig_flowdock.ex
@@ -71,6 +71,9 @@ defmodule Hedwig.Adapters.Flowdock do
       }
     }
 
+    Logger.info "Received message via cast: #{msg.text}"
+    Logger.info "Robot: #{inspect(robot)}"
+
     if msg.text do
       Hedwig.Robot.handle_in(robot, msg)
     end
